66fac42e624eb737af86735763f24c1e4aec9b97
David Jensenius <david@jensenius.com>
2024-08-11 19:21:37 -0400

    Add watch 😂

---
+++ RhizomeWatch Watch App/ContentView.swift
@@ -0,0 +1,74 @@
+//
+//  ContentView.swift
+//  RhizomeTV
+//
+//  Created by David Jensenius on 2024-06-18.
+//
+
+import SwiftUI
+import AVKit
+import AVFoundation
+
+struct ContentView: View {
+    var cameraURL: String
+    var rhizomeSchedule: Appointments?
+    @State var inPlayroom = false
+    @State var path = [Int]()
+
+    var showRhizome: some View {
+        VStack {
+            if inPlayroom {
+                Text("🐕🎉 Rhizome is playing! 🎉🐕")
+                    .padding([.bottom], 20)
+            } else {
+                Text("🐕 Rhizome is not in the playroom 🐕")
+                    .padding([.bottom], 20)
+            }
+            Button {
+                path = [1]
+            } label: {
+                if inPlayroom {
+                    Text("Watch Rhizome")
+                } else {
+                    Text("View Anyway")
+                }
+            }
+        }
+    }
+
+    var body: some View {
+        let asset = AVAsset(url: URL(string: cameraURL)!)
+        let playerItem = AVPlayerItem(asset: asset)
+        let player = AVPlayer(playerItem: playerItem)
+        NavigationStack(path: $path) {
+            HStack {
+                showRhizome
+            }.navigationDestination(for: Int.self) { selection in
+                if selection == 1 {
+                    VideoPlayer(player: player).ignoresSafeArea()
+                        .onAppear { player.play() }
+                }
+            }
+        }
+        .onAppear(perform: parseSchedule)
+    }
+
+    func parseSchedule() {
+        if rhizomeSchedule != nil && rhizomeSchedule?.daycare != nil && rhizomeSchedule?.daycare.count ?? 0 > 0 {
+            for daycare in rhizomeSchedule!.daycare {
+                let startDate = Date(timeIntervalSince1970: TimeInterval((daycare.date / 1000) - (60 * 60)))
+                let endDate = Date(timeIntervalSince1970: TimeInterval((daycare.pickupDate / 1000) + (60 * 60)))
+                if Date.now  > startDate && Date.now < endDate {
+                    inPlayroom = true
+                    path.append(1)
+                } else {
+                    inPlayroom = false
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView(cameraURL: "")
+}
---
+++ RhizomeWatch Watch App/RhizomeWatchApp.swift
@@ -0,0 +1,52 @@
+//
+//  RhizomeWatchApp.swift
+//  RhizomeWatch Watch App
+//
+//  Created by David Jensenius on 2024-08-11.
+//
+
+import SwiftUI
+
+@main
+struct RhizomeWatchWatchAppApp: App {
+    @State private var whereWeAre = WhereWeAre()
+    @State var cameraURL = ""
+    @State var images: [String] = []
+    @State var rhizomeSchedule: Appointments?
+    @State var newsUrl: String?
+
+    @State private var user: String?
+
+    var body: some Scene {
+        WindowGroup {
+            if whereWeAre.loading == true {
+                LoadingView(needLoginView: !whereWeAre.hasKeyChainPassword)
+                    .transition(.opacity.animation(.linear))
+                    .onReceive(NotificationCenter.default.publisher(for: Notification.Name.loginsUpdated)) { object in
+                        if ((object.userInfo?["keysComplete"]) != nil) == true {
+                            if object.object != nil {
+                                let configResponse = object.object! as? LoginResponse
+                                cameraURL = configResponse?.cameraURL ?? ""
+                                images = configResponse?.rhizomeData.photos ?? []
+                                newsUrl = configResponse?.rhizomeData.news ?? nil
+                                rhizomeSchedule = configResponse?.rhizomeSchedule.appointments
+                            }
+                        }
+
+                        if (object.userInfo?["updateKeychain"]) != nil {
+                            whereWeAre.setPassword(password: object.userInfo!["updateKeychain"] as? String ?? "")
+                        }
+
+                        if ((object.userInfo?["keysFailed"]) != nil) == true {
+                            whereWeAre.deleteKeyChainPasword()
+                        }
+                    }
+                } else {
+                    ContentView(cameraURL: cameraURL, rhizomeSchedule: rhizomeSchedule)
+                        .tabItem {
+                            Label("Watch", systemImage: "tv")
+                        }
+                }
+        }
+    }
+}
